d5efb038d01a4a6e742a80e1550b59df2aa7aa0a
yoanndelattre <yoanndelattre21@gmail.com>
2023-05-18 00:26:10 +0200

    remove instgram download function

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ import crop_images
 from log import log
 sys.path.append('source')
 import bing_image
-import instagram_suggestion
 
 env_execute = os.environ.get('ENV_EXECUTE')
 
@@ -14,10 +13,8 @@ output_original = "output_original"
 
 if env_execute == "PROD":
     limit_browser = 100
-    limit_instagram = 10
 else:
     limit_browser = 5
-    limit_instagram = 2
 
 output_resize = "output_resize"
 img_width = 1024
@@ -26,8 +23,6 @@ img_height = 1024
 if query is not None:
     log("-----Download Bing Image-----")
     bing_image.download(query, limit_browser, output_original)
-    log("-----Download Instagram Suggestion-----")
-    instagram_suggestion.download(query, limit_instagram, output_original)
     crop_images.crop_images(query, output_original, output_resize, img_width, img_height)
 else:
     log("query is not set.")
