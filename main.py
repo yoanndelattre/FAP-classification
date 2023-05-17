bf900a03aed04a5f875a1fc8c01c404b72e799e8
yoanndelattre <yoanndelattre21@gmail.com>
2023-05-18 01:11:06 +0200

    add move_and_delete_folder function

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import sys
 import os
 import crop_images
+import move_and_delete_folder
 from log import log
 sys.path.append('source')
 import bing_image
@@ -10,6 +11,8 @@ env_execute = os.environ.get('ENV_EXECUTE')
 query = os.environ.get('QUERY_SEARCH')
 
 output_original = "output_original"
+
+bing_tmp_folder = "bing_tmp_folder"
 
 if env_execute == "PROD":
     limit_browser = 100
@@ -22,7 +25,8 @@ img_height = 1024
 
 if query is not None:
     log("-----Download Bing Image-----")
-    bing_image.download(query, limit_browser, output_original)
+    bing_image.download(query, limit_browser, bing_tmp_folder)
+    move_and_delete_folder.move_and_delete_folder(bing_tmp_folder, output_original, query, "bing")
     crop_images.crop_images(query, output_original, output_resize, img_width, img_height)
 else:
     log("query is not set.")
