ad1e1404c115fa3b141c39ff3ba39d96aed052ca
yoanndelattre <yoanndelattre21@gmail.com>
2023-05-18 18:51:05 +0200

    rename output folder

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ env_execute = os.environ.get('ENV_EXECUTE')
 
 query = os.environ.get('QUERY_SEARCH')
 
-output_original = "output_original"
+output_download_folder = "output_download"
 
 bing_tmp_folder = "bing_tmp_folder"
 twitter_suggestion_tmp_folder = "twitter_suggestion_tmp_folder"
@@ -21,7 +21,7 @@ if env_execute == "PROD":
 else:
     limit_browser = 5
 
-output_resize = "output_resize"
+output_resize_folder = "output_resize"
 img_width = 1024
 img_height = 1024
 
@@ -29,14 +29,14 @@ if query is not None:
     log("-----Download Bing Image-----")
     bing_search.download(query, limit_browser, bing_tmp_folder)
     extract.rename_tmpfiles(bing_tmp_folder, query, "bing")
-    extract.move_and_delete_tmp_folder(bing_tmp_folder, output_original, query)
+    extract.move_and_delete_tmp_folder(bing_tmp_folder, output_download_folder, query)
 
     log("-----Download Twitter Suggestion-----")
     twitter_suggestion.download(query, twitter_suggestion_tmp_folder)
     extract.rename_tmpfiles(twitter_suggestion_tmp_folder, query, "twitter")
-    extract.move_and_delete_tmp_folder(twitter_suggestion_tmp_folder, output_original, query)
+    extract.move_and_delete_tmp_folder(twitter_suggestion_tmp_folder, output_download_folder, query)
 
     log("-----Crop all images-----")
-    crop_images.crop_images(query, output_original, output_resize, img_width, img_height)
+    crop_images.crop_images(query, output_download_folder, output_resize, img_width, img_height)
 else:
     log("query is not set.")
