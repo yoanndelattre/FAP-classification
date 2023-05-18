15318e62020064fdc66d8968cca74cfbae9ec0c1
yoanndelattre <yoanndelattre21@gmail.com>
2023-05-18 23:28:04 +0200

    remove pinterest module

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,6 @@
 FROM python:3.10-slim
 RUN apt update && \
-    apt upgrade -y && \
-    apt install -y wget gnupg && \
-    wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | apt-key add - && \
-    sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list' && \
-    apt update && \
-    apt install -y google-chrome-stable
+    apt upgrade -y
 COPY requirements.txt requirements.txt
 RUN pip3 install -r requirements.txt
 ENV APP_HOME /app
--- main.py
+++ main.py
@@ -2,11 +2,9 @@ import sys
 import os
 import crop_images
 import extract
-import setup_chrome
 from log import log
 sys.path.append('downloaders')
 import bing_search
-import pinterest
 
 env_execute = os.environ.get('ENV_EXECUTE')
 
@@ -15,30 +13,21 @@ query = os.environ.get('QUERY_SEARCH')
 output_download_folder = "output_download"
 
 bing_tmp_folder = "bing_tmp_folder"
-pinterest_suggestion_tmp_folder = "pinterest_suggestion_tmp_folder"
 
 if env_execute == "PROD":
     limit_browser = 100
-    limit_pinterest = 10
 else:
     limit_browser = 5
-    limit_pinterest = 5
 
 output_resize_folder = "output_resize"
 img_width = 1024
 img_height = 1024
 
 if query is not None:
-    setup_chrome.chromedriver()
     log("-----Download Bing Image-----")
     bing_search.download(query, limit_browser, bing_tmp_folder)
     extract.rename_tmpfiles(bing_tmp_folder, query, "bing")
     extract.move_and_delete_tmp_folder(bing_tmp_folder, output_download_folder, query)
-
-    log("-----Download Pinterest Suggestion-----")
-    pinterest.download(query, limit_pinterest, pinterest_suggestion_tmp_folder)
-    extract.rename_tmpfiles(pinterest_suggestion_tmp_folder, query, "pinterest")
-    extract.move_and_delete_tmp_folder(pinterest_suggestion_tmp_folder, output_download_folder, query)
 
     log("-----Crop all images-----")
     crop_images.crop_images(query, output_download_folder, output_resize_folder, img_width, img_height)
